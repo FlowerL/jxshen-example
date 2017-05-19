a6e3c78ff1ece8c6716935c13a3131c59978831f
沈健翔 <vxlang78@163.com>
2017-05-19 23:23:58 +0800

    File read

---
+++ nio/src/main/java/com/jxshen/example/nio/file/FileReadExample.java
@@ -0,0 +1,12 @@
+package com.jxshen.example.nio.file;
+
+import java.io.InputStream;
+
+public class FileReadExample {
+
+    private static final String FILE_NAME = "D:/example.txt";
+    
+    public void transational() {
+        InputStream in = null;
+    }
+}
