7e40159edf7bd925d165f1aae8fc43b655602d97
沈健翔 <vxlang78@163.com>
2017-05-20 22:40:45 +0800

    nio client & bio server

---
+++ nio/src/main/java/com/jxshen/example/nio/cs/ServerClientExample.java
@@ -0,0 +1,86 @@
+package com.jxshen.example.nio.cs;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
+
+public class ServerClientExample {
+
+    public void NioClient() {
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+        SocketChannel sc = null;
+        try {
+            sc = SocketChannel.open();
+            sc.configureBlocking(false);
+            sc.connect(new InetSocketAddress("localhost", 8080));
+            
+            if (sc.finishConnect()) {
+                int i = 0;
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = String.format("I'm [%d]th information from client", i++);
+                    buf.clear();
+                    buf.put(info.getBytes());
+                    buf.flip();
+                    while (buf.hasRemaining()) {
+                        System.out.println(buf);
+                        sc.write(buf);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (sc != null) {
+                    sc.close();
+                }
+            } catch(IOException e) {
+                
+            }
+        }
+    }
+    
+    public void IOServer() {
+        ServerSocket serverSocket = null;
+        InputStream in = null;
+        try {
+            serverSocket = new ServerSocket(8080);
+            int recvMsgSize = 0;
+            byte[] recvbuf = new byte[2024];
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                SocketAddress clientAddress = clientSocket.getRemoteSocketAddress();
+                System.out.println("Handling client at " + clientAddress);
+                in = new BufferedInputStream(clientSocket.getInputStream());
+                while ((recvMsgSize = in.read(recvbuf)) != -1) {
+                    byte[] temp = new byte[recvMsgSize];
+                    System.arraycopy(recvbuf, 0, temp, 0, recvMsgSize);
+                    System.out.println(new String(temp));
+                }
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {}
+        }
+    }
+}
--- nio/src/main/java/com/jxshen/example/nio/file/FileReadExample.java
+++ nio/src/main/java/com/jxshen/example/nio/file/FileReadExample.java
@@ -1,6 +1,13 @@
 package com.jxshen.example.nio.file;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileReadExample {
 
@@ -8,5 +15,59 @@ public class FileReadExample {
     
     public void transational() {
         InputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(FILE_NAME));
+            byte[] buf = new byte[1024];
+            int bytesRead = in.read(buf);
+            while (bytesRead!= -1) {
+                for (int i = 0; i < bytesRead; i++) {
+                    System.out.println((char)buf[i]);
+                }
+                bytesRead = in.read(buf);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public void nio() {
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(FILE_NAME,"rw");
+            FileChannel fc = raf.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(1024);
+            int bytesRead = fc.read(buf);
+            while (bytesRead != -1) {
+                buf.flip();
+                while (buf.hasRemaining()) {
+                    System.out.println((char)buf.get());
+                }
+                buf.compact();
+                bytesRead = fc.read(buf);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            if (raf != null) {
+                try {
+                    raf.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                }
+            }
+        }
     }
 }
