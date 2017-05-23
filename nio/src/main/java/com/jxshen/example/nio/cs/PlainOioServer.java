f0afb9641cbb2a8d2f3917343c3db2a0c8a77a38
沈健翔 <vxlang78@163.com>
2017-05-23 23:22:31 +0800

    tiny

--- nio/src/main/java/com/jxshen/example/nio/cs/PlainOioServer.java
+++ nio/src/main/java/com/jxshen/example/nio/cs/PlainOioServer.java
@@ -1,8 +1,10 @@
 package com.jxshen.example.nio.cs;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 /**
  * reference from: Netty In Action
@@ -13,6 +15,19 @@ public class PlainOioServer {
         final ServerSocket serverSock = new ServerSocket(port);
         while(true) {
             final Socket clientSock = serverSock.accept();
+            System.out.println(String.format("Accepted connection from [%s]", clientSock));
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    OutputStream os = null;
+                    try {
+                        os = clientSock.getOutputStream();
+                        os.write("Hello!\r\n".getBytes(Charset.forName("UTF-8")));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
     }
 }
