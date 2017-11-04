b924a361c596c2e636904102154675ea593d9eb8
FLowerL <vxlang78@163.com>
2017-11-04 16:52:46 +0800

    SingleNumber136

---
+++ leetcode/src/main/java/com/jxshen/example/leetcode/SingleNumber136.java
@@ -0,0 +1,11 @@
+package com.jxshen.example.leetcode;
+
+public class SingleNumber136 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
