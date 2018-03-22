c6110a487376db6062c070c3ccf9209a807e1370
沈健翔 <qixing.sjx@raycloud.com>
2018-03-22 13:36:13 +0800

    CheckClassLoader

--- jdk/src/main/java/com/jxshen/example/jdk/classloader/CheckClassLoader.java
+++ jdk/src/main/java/com/jxshen/example/jdk/classloader/CheckClassLoader.java
@@ -10,7 +10,7 @@ import java.security.ProtectionDomain;
  * 这就是StringUtils这个类所在的jar包，一目了然。但是有一个限制经由系统类加载器所加载的类不能使用该方法来获取路径，
  * 假如你对String类做上面的测试就会发生空指针异常，因为所得到的CodeSource为空，你可以在程序启动的时候使用
  * -verbose来查看系统类的装载信息，使用方式有如：
- * java -verbose MyApp
+ * java -verbose MyApp xiugai
  *
  * @reference from: http://blog.csdn.net/yicong406880638/article/details/48295029
  */
